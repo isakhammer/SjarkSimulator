71e684570650c781538e6f7d28779c46cf721ec1
Isak Hammer <isakh@stud.ntnu.no>
2025-12-05 19:17:28 +0100

    Constructed basic launch file for throttle.cpp

---
+++ src/controller/launch/controller_launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+            Node(
+                package='controller',
+                namespace="controller_ns",
+                executable='throttle',
+                name='throttle'
+                )
+    ])
