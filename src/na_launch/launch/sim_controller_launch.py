b18777098da7ddff7406bf155d7b6d0bdda19686
Isak Hammer <isakh@stud.ntnu.no>
2025-12-10 21:15:49 +0100

    A launch file for sim and control

---
+++ src/na_launch/launch/sim_controller_launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='na_controller',
+            namespace='controller_ns',
+            executable='throttle',
+            name='throttle'
+        ),
+        Node(
+            package='na_sim',
+            namespace='sim_ns',
+            executable='sim_node',
+            name='sim_node'
+        )
+    ])
